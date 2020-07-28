df60117ee6eb4c09adcb22b792c5da135e324c8a
David <jobroman83@gmail.com>
2020-07-28 16:43:39 +0200

    T-1 Adding json fake server

---
+++ src/components/EditarProducto.js
@@ -0,0 +1,42 @@
+import React from 'react'
+
+const EditarProducto = () => {
+    return(
+    <div className='row justify-content-center'>
+        <div className='col-md-8'>
+            <div className='card'>
+                <div className='card-body'>
+                    <h2 className='text-center mb-4 font-weight-bold'>
+                        Editar Nuevo Producto
+                    </h2>
+                    <form>
+                        <div className='form-group'>
+                            <input
+                            type='text'
+                            className='form-control'
+                            placeholder='Nombre del Producto'
+                            name='nombre'
+                            />
+                        </div>
+                        <div className='form-group'>
+                            <input
+                            type='number'
+                            className='form-control'
+                            placeholder='Precio del Producto'
+                            name='precio'
+                            />
+                        </div>
+                        <button 
+                            type='submit'
+                            className='btn btn-primary font-weight-bold text-uppercase d-block w-100'>
+                            Guardar Cambios
+                        </button>
+                    </form>
+                </div>
+            </div>   
+        </div>
+    </div>
+    )
+}
+
+export default EditarProducto
