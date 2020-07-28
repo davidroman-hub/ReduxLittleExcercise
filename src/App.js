de1259e0444a6aa1bc5bde190c066f16f9e05d1a
David <jobroman83@gmail.com>
2020-07-28 17:17:28 +0200

    T-2 Adding configure redux

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import Header from './components/Header'
 import Productos from './components/Productos';
 import NuevoProducto from './components/NuevoProducto';
@@ -6,10 +6,16 @@ import EditarProducto from './components/EditarProducto';
 
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 
+//REDUX
+
+import { Provider } from 'react-redux'
+import store from './store'
+
 
 function App() {
   return (
     <Router>
+      <Provider store={store}>
         <h1>Redux </h1>
       <Header/>
       <div className='container mt-5'>
@@ -19,6 +25,7 @@ function App() {
            <Route exact path='/productos/editar/:id' component={EditarProducto} />
         </Switch>
       </div>
+      </Provider>
     </Router>
       
   );
