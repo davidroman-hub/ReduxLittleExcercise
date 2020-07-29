e5a15fdb3ac87530f77d27fa2f0574924e43a460
David <jobroman83@gmail.com>
2020-07-29 15:54:28 +0200

    T-5 agregando un Nuevo producto al state

--- src/actions/productoActions.js
+++ src/actions/productoActions.js
@@ -8,8 +8,35 @@ import {
 // Crear nuevos productos
 
 export function crearNuevoProductoAction(producto) {
-    return () => {
+    return (dispatch) => {
       //  console.log('desde action')
-        console.log(producto)
+       // console.log(producto)
+       dispatch( agregarProducto());
+
+       try {
+           //dispatch manda a llamar a las funciones
+
+           dispatch(agregarProductoExito(producto))
+       } catch (error) {
+           dispatch(agregarProductoError(true))
+       }
     }
+}
+
+const agregarProducto = () => ({
+    type:AGREGAR_PRODUCTO,
+     payload: true
+
+})
+
+// si el producto se guarda en la base de datos o si hay un error
+
+const agregarProductoExito = producto => ({
+    type:AGREGAR_PRODUCTO_EXITO,
+    // para a modificar state se pasa un payload
+    payload:producto
+})
+
+const agregarProductoError = () => {
+    //
 }
